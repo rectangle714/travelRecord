48f9349021c634f16783fedbdd24e006691b3b71
rectangle714 <62207860+rectangle714@users.noreply.github.com>
2022-08-03 14:06:04 +0900

    Add files via upload

---
+++ travelRecord/src/main/java/com/prj/travelRecord/controller/helloController.java
@@ -0,0 +1,9 @@
+package com.prj.travelRecord.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class helloController {
+	
+	
+}
---
+++ travelRecord/src/test/java/com/prj/travelRecord/TravelRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prj.travelRecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelRecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
