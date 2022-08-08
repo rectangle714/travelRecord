8247eb2569d8eefef092cea22bef8f0db737fc66
rectangle714 <62207860+rectangle714@users.noreply.github.com>
2022-08-08 09:33:38 +0900

    back 커밋

---
+++ travelRecord/src/main/java/com/prj/travelRecord/TravelRecordApplication.java
@@ -0,0 +1,13 @@
+package com.prj.travelRecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelRecordApplication.class, args);
+	}
+	
+}
